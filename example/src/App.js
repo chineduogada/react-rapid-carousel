48f02eaa0785c4fb89cd6f96a8a7d369525c8527
richCode <chineduogada@gmail.com>
2020-10-09 15:39:02 +0100

    chore: update version

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { Slider, Fader } from 'react-rapid-carousel'
 import 'react-rapid-carousel/dist/index.css'
 
 const heroes = [
-  <div key={4}>4</div>,
+  <div key={4}>4 :yes</div>,
   <div key={5}>5 :joy</div>,
   <div key={6}>6ix</div>
 ]
